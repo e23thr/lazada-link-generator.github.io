207f40b57c10957329c0680af8d277141cd34855
Praphan Oranphanlert <e23thr@gmail.com>
2021-09-03 06:13:44 +0000

    fix test error

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders URL ของคุณ', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/URL ของคุณ/i);
   expect(linkElement).toBeInTheDocument();
 });
